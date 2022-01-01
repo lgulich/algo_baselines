428df905bdc50e699320ea258c0aa1a664a68bf2
lgulich <lgulich@ethz.ch>
2022-01-01 15:34:37 +0100

    Add Dijkstra shortest path

--- include/graph/shortest_path.h
+++ include/graph/shortest_path.h
@@ -2,14 +2,16 @@
 #define GRAPH_SHORTEST_PATH_H_
 
 #include <limits>
+#include <queue>
+#include <unordered_set>
 
 #include "graph/types.h"
 
 // T~O(N*E), S~(N)
 // Find the shortest path to all nodes in graph from a start node. Additionally
 // also finds nodes in negative cycles (shortest path = -inf).
-inline vector<double> shortestPathBellford(const WeightedAdjacencyList &graph,
-                                           const int start) {
+inline vector<double> shortestPathBellford(
+    const WeightedAdjacencyList& graph, const int start) {
   constexpr double kInf = std::numeric_limits<double>::infinity();
 
   if (graph.empty()) {
@@ -27,7 +29,7 @@ inline vector<double> shortestPathBellford(const WeightedAdjacencyList &graph,
   // T~O(N*E), S~(1)
   for (int i = 0; i < graph.size() - 1; ++i) {
     for (int src = 0; src < graph.size(); ++src) {
-      for (const auto &edge : graph[src]) {
+      for (const auto& edge : graph[src]) {
         costs[edge.dst] = std::min(costs[edge.dst], costs[src] + edge.weight);
       }
     }
@@ -37,7 +39,7 @@ inline vector<double> shortestPathBellford(const WeightedAdjacencyList &graph,
   // T~O(N*E), S~(1)
   for (int i = 0; i < graph.size() - 1; ++i) {
     for (int src = 0; src < graph.size(); ++src) {
-      for (const auto &edge : graph[src]) {
+      for (const auto& edge : graph[src]) {
         if (costs[src] + edge.weight < costs[edge.dst]) {
           costs[edge.dst] = -kInf;
         }
@@ -48,4 +50,71 @@ inline vector<double> shortestPathBellford(const WeightedAdjacencyList &graph,
   return costs;
 }
 
-#endif // GRAPH_SHORTEST_PATH_H_
+// T~O(E*log(N)), S~(N)
+// Find the shortest path to all nodes in graph from a start node. Additionally
+// also finds nodes in negative cycles (shortest path = -inf).
+inline vector<int> shortestPathDijkstra(
+    const WeightedAdjacencyList& graph, const int start, const int end) {
+  struct NodeAndCost {
+    int node;
+    double cost;
+
+    bool operator<(const NodeAndCost& rhs) const {
+      return this->cost < rhs.cost;
+    }
+  };
+
+  // Start or end are not in graph.
+  if (start >= graph.size() || end >= graph.size()) {
+    return {};
+  }
+
+  // T~O(N), S~(N)
+  std::priority_queue<NodeAndCost> open;
+  open.push({start, 0.});
+
+  vector<int> parent(graph.size(), -1);
+  vector<double> cost(graph.size(), std::numeric_limits<double>::infinity());
+  cost[start] = 0.;
+
+  // Label correcting step
+  // T~O(E*log(N)), S~(1)
+  while (!open.empty()) {
+    const int current = open.top().node;
+    open.pop();
+
+    for (const auto& edge : graph[current]) {
+      const double dst_cost = cost[current] + edge.weight;
+
+      if (dst_cost < cost[edge.dst]) {
+        cost[edge.dst] = dst_cost;
+        parent[edge.dst] = current;
+        // T~O(log(N))
+        open.push({edge.dst, dst_cost});
+      }
+    }
+  }
+
+  // Backtrack
+  // T~O(N), S~(N)
+  vector<int> path;
+  int current = end;
+
+  while (parent[current] != -1) {
+    path.push_back(current);
+    current = parent[current];
+  }
+  path.push_back(current);
+
+  // If no path is found.
+  if (path.back() != start) {
+    return {};
+  }
+
+  // T~O(N), S~(1)
+  std::reverse(path.begin(), path.end());
+
+  return path;
+}
+
+#endif  // GRAPH_SHORTEST_PATH_H_
--- test/graph/shortest_path_test.cc
+++ test/graph/shortest_path_test.cc
@@ -38,17 +38,73 @@ TEST(ShortestPathBellford, StartNodeNotInGraph) {
 
 TEST(ShortestPathBellford, FullGraph) {
   const WeightedAdjacencyList graph{
-      {{1, 1.}, {2, 1.}, {7, 2.}}, // 0
-      {{3, 4.}},                   // 1
-      {{1, 1.}},                   // 2
-      {{2, -6}, {4, 1.}, {5, 1.}}, // 3
-      {{5, 1.}, {6, 1.}},          // 4
-      {{6, 1.}},                   // 5
-      {},                          // 6
-      {},                          // 7
+      {{1, 1.}, {2, 1.}, {7, 2.}},  // 0
+      {{3, 4.}},                    // 1
+      {{1, 1.}},                    // 2
+      {{2, -6}, {4, 1.}, {5, 1.}},  // 3
+      {{5, 1.}, {6, 1.}},           // 4
+      {{6, 1.}},                    // 5
+      {},                           // 6
+      {},                           // 7
   };
 
   const vector<double> costs = shortestPathBellford(graph, 0);
-  print(costs);
   expect_eq(costs, {0., -kInf, -kInf, -kInf, -kInf, -kInf, -kInf, 2.});
+}
+
+TEST(ShortestPathDijkstra, EmptyGraph) {
+  const WeightedAdjacencyList graph;
+  const vector<int> path = shortestPathDijkstra(graph, 0, 1);
+  expect_eq(path, {});
+}
+
+TEST(ShortestPathDijkstra, LinearGraph) {
+  const WeightedAdjacencyList graph{
+      {{1, 1.0}},
+      {{2, 2.0}},
+      {{3, 3.0}},
+      {},
+  };
+
+  const vector<int> path = shortestPathDijkstra(graph, 0, 3);
+  expect_eq(path, {0, 1, 2, 3});
+}
+
+TEST(ShortestPathDijkstra, StartNodeNotInGraph) {
+  const WeightedAdjacencyList graph{
+      {{1, 1.0}},
+      {{2, 2.0}},
+      {{3, 3.0}},
+      {},
+  };
+
+  const vector<int> path = shortestPathDijkstra(graph, 4, 2);
+  expect_eq(path, {});
+}
+
+TEST(ShortestPathDijkstra, FullGraph) {
+  const WeightedAdjacencyList graph{
+      {{1, 1.}, {2, 2.}},  // 0
+      {{3, 1.}},           // 1
+      {{3, 1.}},           // 2
+      {{4, 1}, {5, 1.}},   // 3
+      {{6, 2.}},           // 4
+      {{6, 1.}},           // 5
+      {},                  // 6
+  };
+
+  const vector<int> path = shortestPathDijkstra(graph, 0, 6);
+  expect_eq(path, {0, 1, 3, 5, 6});
+}
+
+TEST(ShortestPathDijkstra, PathNotExisting) {
+  const WeightedAdjacencyList graph{
+      {{1, 1.}},  // 0
+      {{2, 2.}},  // 1
+      {{3, 3.}},  // 2
+      {},         // 3
+  };
+
+  const vector<int> path = shortestPathDijkstra(graph, 3, 0);
+  expect_eq(path, {});
 }
